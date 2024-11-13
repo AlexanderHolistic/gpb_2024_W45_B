80c198369a543e5391c120d29c0a272faeef1819
Sascha Kleinen <saschakleinen91@gmail.com>
2024-11-13 17:17:34 +0100

    Bootstrap zuhause installiert, da Berechtigungen in der Schule fehlen.

--- datenbank/dbstruktur.sql
+++ datenbank/dbstruktur.sql
@@ -15,14 +15,14 @@ create table user
 
 
 create table notizen 
-{
+(
 
   id int auto_increment primary key,
   titel varchar(255),
   inhalt varchar(255),
   user_id int not null,
-   Datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-   FOREIGN KEY (user_id) REFERENCES user (id)
+  Datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES user (id)
 
 
-}
+)

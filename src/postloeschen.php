3f8689c7db608d1055dc45157a2b43d957c6a3b9
Dennis Maier <tnnk@outlook.com>
2024-11-14 10:22:35 +0100

    check nach id

--- src/postloeschen.php
+++ src/postloeschen.php
@@ -1,5 +1,9 @@
 <?php
 $id=isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if($id> 0){
+    header('Location: index.php');
+    exit;
+}
 
 require_once 'db.php';
 $stmt=$db->prepare("DELETE FROM notizen WHERE `notizen`.`id` = ?");
